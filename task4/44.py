fb6205a3450a386f5229c9925ed894edf8b1e412
exemple3 <ilya_kuznetcov_school@mail.ru>
2025-09-28 19:04:01 +0800

    44.py

--- task4/44.py
+++ task4/44.py
@@ -1,15 +1,27 @@
-def f(p):
-    n = len(p)
+def f(a):
+    n = len(a)
     decode = [0] * n
     for i in range(n):
-        decode[p[i] - 1] = i
+        decode[a[i] - 1] = i
     return decode
+# функция декодировки
 
-n, k = map(int, input().split())
-p = list(map(int, input().split()))
+a = input().split()
+n = int(a[0])
+k = int(a[1])
+# ввод длины слова и количества шифрования
+
+p = []
+    for j in input().split():
+        p.append(int(j))
 a = input()
+# ввод перестановки и слова
 
 decode = f(p)
-for _ in range(k):
-    a = ''.join(a[decode[i]] for i in range(n))
+for i in range(k):
+    new_a = ''
+    for i in range(n):
+        new_a += a[decode[i]]
+    a = new_a
+# изменяет слово и заменяет им старое
 print(a)
