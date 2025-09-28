027d459494674a1643d4210fdbb1c89b7da5cb5c
exemple3 <ilya_kuznetcov_school@mail.ru>
2025-09-28 17:51:23 +0800

    22.py

--- task2/22.py
+++ task2/22.py
@@ -3,14 +3,25 @@ def dfs(g, graph, visited):
     for v in graph[g]:
         if not visited[v]:
             dfs(v, graph, visited)
+# обход графа, помечает комп. связности
 
-N, M = map(int, input().split())
+a = input().split()
+N = int(a[0])
+M = int(a[1])
+# вершины и ребра
 
-graph = {i: [] for i in range(1, N + 1)}
-for _ in range(M):
-    u, v = map(int, input().split())
-    graph[u].append(v)
-    graph[v].append(u)
+graph = {}
+for i in range(1, N + 1):
+    graph[i] = []
+# словарь, соседние вершины: вершина i
+
+for i in range(M):
+    b = input().split()
+    r = int(b[0])
+    v = int(b[1])
+    graph[r].append(v)
+    graph[v].append(r)
+# счит. ребра и добавляем в обе стороны
 
 visited = [False] * (N + 1)
 comp = 0
@@ -18,5 +29,6 @@ comp = 0
 for i in range(1, N + 1):
     if not visited[i]:
         dfs(i, graph, visited)
-        comp += 1      
+        comp += 1    
+# проходим по верш., если не посещена, то принадлежит к др. компоненте
 print(comp - 1)
